17489a358dedf304c267b502be37c21f81cbe5d2
Prashant Ghildiyal <60953820+pghildiyal@users.noreply.github.com>
2023-01-02 17:19:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var text4 = text3+ " "+makeid(40);
 var logs = ["i 1", "am 1", "here 1", "hello 1", "and 1", "you 1", "know 1", "who 1", "i 2", "am 2", "here 2", "hello 2", "hello 2", "and 2", "you 2", "know 2", "who 2", "i 3", "am 3", "here 3", "hello 3", "and 3", "hello 3", "and 3", "you 3", "know 3", "who 3", "i 4", "am 4", "here 4", "hello 4", "and 4", "we 4", "hello 4", "and 4", "you 4", "know 4", "who 4", "i 5", "am 5", "here 5", "hello 5", "or 5", "not 5", "and 5", "we 5", "hello 5", "and 5", "you 5", "know 5", "who 5", "i 6", "am 6", "here 6", "hello 6", "or 6", "not 6", "and 6", "hello 6", "and 6", "you 6", "know 6", "who 6", "i 7", "am 7", "here 7", "hello 7", "and 7", "we 7", "are 7", "here1 7", "hello 7", "and 7", "you 7", "know 7", "who 7", "i 8", "am 8", "here 8", "hello 8", "and 8", "we 8", "miss 8", "are 8", "here1 8", "hello 8", "and 8", "you 8", "know 8", "who 8"]
 var i = 0
 app.get('/', function(req, res){
+    console.log("received request");
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(cities));
     res.end();
@@ -30,7 +31,7 @@ setInterval(function(str1, str2) {
     else if (ran > 0.25)
         text = makeid(40)+ ""+makeid(40)
   //console.log(str1 + "" + str2 + "" + text+ "" +makeid(40) + "args2" + process.argv);
-  console.log(logs[i])
+  //console.log(logs[i])
   i++
   if (i == logs.length) i = 0
 }, 1, "Hello.", "How are you?");
